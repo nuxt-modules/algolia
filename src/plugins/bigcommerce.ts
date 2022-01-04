9a927262865665e7d6328e2d171b1eb863a1814f
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-04 20:29:08 +0100

    feat: remove unused files

--- src/plugins/bigcommerce.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtPlugin, NuxtApp } from '#app'
-import { BigCommerceClientApi } from '../client';
-
-export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
-  const bigcommerceClientApi = new BigCommerceClientApi();
-
-  nuxtApp.provide('bigcommerce', bigcommerceClientApi)
-})
--- src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const PAYMENT_URL = 'https://payments.bigcommerce.com'
-export const REST_API_URL = 'https://api.bigcommerce.com'
