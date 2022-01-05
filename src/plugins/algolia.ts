4944e7ae33575a473ef76743856ad46de90a1990
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-05 11:21:54 +0100

    feat: final setup for release

--- src/plugins/algolia.ts
+++ src/plugins/algolia.ts
@@ -1,4 +1,5 @@
-import algoliasearch from 'algoliasearch/lite'
+// Have to import algoliasearch directly from esm.browser because algoliasearch by default provides umd.js file which causes Nuxt to throw error
+import algoliasearch from 'algoliasearch/dist/algoliasearch-lite.esm.browser'
 import { defineNuxtPlugin, NuxtApp } from '#app'
 
 export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
