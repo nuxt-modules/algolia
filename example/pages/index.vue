c7871b97a33462799c74007927c01e0685751ee9
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-10 08:57:28 +0100

    feat: update example with new data

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    {{ result }}
+    {{ result?.hits }}
   </div>
 </template>
 
 <script lang="ts" setup>
 const { result, search } = useSearch('test_index')
+const algoliaIndex = useInitIndex('test')
 const algolia = useAlgolia()
 
 console.log(algolia)
+console.log(algoliaIndex)
 
 onMounted(async () => {
-  await search({ query: 'Samsung' })
+  await search({ query: 'Samsung', requestOptions: { filters: 'objectID:ecommerce-sample-data-99' } })
 })
 </script>
