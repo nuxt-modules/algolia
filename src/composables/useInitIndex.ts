0655cd888124806faf392f49ec9aec94a58a1724
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-10 08:50:14 +0100

    feat: new composable useInitIndex

---
+++ src/composables/useInitIndex.ts
@@ -0,0 +1,8 @@
+import { useAlgolia } from './useAlgolia'
+
+export const useInitIndex = (indexName: string) => {
+  const algolia = useAlgolia();
+  const algoliaIndex = algolia.initIndex(indexName);
+
+  return algoliaIndex;
+}
