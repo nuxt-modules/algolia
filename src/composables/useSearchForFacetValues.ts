f966a408df0e27d0defb3ee1d8e26e0ea3be7069
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-13 09:08:43 +0100

    feat: lint

--- src/composables/useSearchForFacetValues.ts
+++ src/composables/useSearchForFacetValues.ts
@@ -1,14 +1,14 @@
 import { computed } from 'vue'
-import { useInitIndex } from "./useInitIndex"
-import { useState } from "#app";
-import { SearchForFacetValuesParams, SearchForFacetValuesResponse } from "../types";
+import { SearchForFacetValuesParams, SearchForFacetValuesResponse } from '../types'
+import { useInitIndex } from './useInitIndex'
+import { useState } from '#app'
 
 export const useSearchForFacetValues = (indexName: string) => {
-  const algoliaIndex = useInitIndex(indexName);
+  const algoliaIndex = useInitIndex(indexName)
   const result = useState<SearchForFacetValuesResponse>(`${indexName}-search-for-facet-values-result`, () => null as any)
 
   const search = async ({ facet, requestOptions }: SearchForFacetValuesParams) => {
-    const { name, query } = facet;
+    const { name, query } = facet
     const searchForFacetValuesResult = await algoliaIndex.searchForFacetValues(name, query, requestOptions)
     result.value = searchForFacetValuesResult
     return searchForFacetValuesResult
