c31d411f701917ac34fb265945b3cbac55ae6466
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-18 10:12:13 +0100

    feat: refactor

--- src/plugins/algolia.ts
+++ src/plugins/algolia.ts
@@ -1,11 +1,11 @@
-// Have to import algoliasearch directly from esm.browser because algoliasearch by default provides umd.js file which causes Nuxt to throw error
-// import algoliasearch from 'algoliasearch/dist/algoliasearch-lite.esm.browser'
 import { defineNuxtPlugin, NuxtApp } from '#app'
 
 export default defineNuxtPlugin(async (nuxtApp: NuxtApp) => {
   const { applicationId, apiKey, lite } = nuxtApp.payload.config.algolia
+  // Have to import algoliasearch directly from esm.browser because algoliasearch by default provides umd.js file which causes Nuxt to throw error
+  // Also, cannot use simple string interpolation due to error 'Cannot read property 'stubModule' of undefined'
   const algoliasearch = lite ? await import('algoliasearch/dist/algoliasearch-lite.esm.browser').then(lib => lib.default || lib) : await import('algoliasearch/dist/algoliasearch.esm.browser').then(lib => lib.default || lib)
-  const algoliaSearch = algoliasearch(applicationId, apiKey)
+  const algoliaSearchClient = algoliasearch(applicationId, apiKey)
 
-  nuxtApp.provide('algolia', algoliaSearch)
+  nuxtApp.provide('algolia', algoliaSearchClient)
 })
