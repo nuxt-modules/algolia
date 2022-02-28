43d892f7c040b05875ac98c33a48a0cb27b02e9f
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2022-02-28 10:04:28 +0100

    fix: add types for composables and lint

--- src/runtime/composables/useSearch.ts
+++ src/runtime/composables/useSearch.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-redeclare */
 import { computed } from 'vue'
 import type { RequestOptions } from '@algolia/transporter'
 import type { SearchOptions, SearchResponse } from '@algolia/client-search'
+import type { ComputedRef } from 'vue'
+import type { AlgoliaIndices } from '../../types'
 import { useInitIndex } from './useInitIndex'
 import { useState } from '#app'
 
@@ -19,9 +22,16 @@ export type SearchForFacetValuesParams = {
   [key: string]: any;
 }
 
-export const useSearch = <T>(indexName: string) => {
+export type UseSearchReturnType<T> = {
+  result: ComputedRef<SearchResponse<T>>,
+  search: (params: SearchParams) => Promise<SearchResponse<T>>,
+}
+
+export function useSearch<K extends keyof AlgoliaIndices>(indexName: K): UseSearchReturnType<AlgoliaIndices[K]>
+export function useSearch<T>(indexName: string): UseSearchReturnType<T>
+export function useSearch (indexName: string) {
   const algoliaIndex = useInitIndex(indexName)
-  const result = useState<SearchResponse<T>>(`${indexName}-search-result`, () => null as any)
+  const result = useState(`${indexName}-search-result`, () => null)
 
   const search = async ({ query, requestOptions }: SearchParams) => {
     const searchResult = await algoliaIndex.search(query, requestOptions)
