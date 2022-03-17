31be1e62bb85b4c0f8e0ef59d1395c0fa01f6072
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2022-03-17 13:25:37 +0100

    shorten logic of composable
    just a suggestion

--- src/runtime/composables/useAlgoliaRecommend.ts
+++ src/runtime/composables/useAlgoliaRecommend.ts
@@ -15,20 +15,18 @@ export type UseAlgoliaRecommend<T> = {
 }
 
 export function useAlgoliaRecommend<T>(): UseAlgoliaRecommend<T> {
-  const nuxtApp = useNuxtApp()
-  const { algolia } = useRuntimeConfig()
-  const isRecommendEnabled = algolia.recommend
-  if (!isRecommendEnabled) {
-    console.error('`[@nuxtjs/algolia]` Cannot call useAlgoliaRecommend composable due to missing `algolia.recommend` option')
+  const { $algoliaRecommend } = useNuxtApp()
+  
+  if (!$algoliaRecommend) {
+    throw new Error('`[@nuxtjs/algolia]` Cannot call useAlgoliaRecommend composable due to missing `algolia.recommend` option.')
   }
-  const algoliaRecommend: RecommendClient = nuxtApp.$algoliaRecommend
+  
   const result = useState('recommend-result', () => null)
 
   const get = async ({ queries, requestOptions }: RecommendParams) => {
-    if (!isRecommendEnabled) throw new Error('`[@nuxtjs/algolia]` Cannot call get method from useAlgoliaRecommend composable due to missing `algolia.recommend` option')
-    const getRecommendationResult = await algoliaRecommend.getRecommendations<T>(queries, requestOptions)
-    result.value = getRecommendationResult
-    return getRecommendationResult
+    result.value = await algoliaRecommend.getRecommendations<T>(queries, requestOptions)
+    
+    return result.value
   }
 
   return {
