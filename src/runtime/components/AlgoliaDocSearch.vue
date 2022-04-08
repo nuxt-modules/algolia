c5519507f02fb37ac896486665e5763654c2edf9
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-08 17:00:48 +0200

    feat(doc-search): update DocSearch component

--- src/runtime/components/AlgoliaDocSearch.vue
+++ src/runtime/components/AlgoliaDocSearch.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="docsearch">
-    <button type="button" class="DocSearch DocSearch-Button justify-end" aria-label="Search">
-      <svg width="20" height="20" class="d-icon m-auto" viewBox="0 0 20 20">
-        <path
-          d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
-          stroke="currentColor"
-          fill="none"
-          fill-rule="evenodd"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </button>
+    <button type="button" class="DocSearch DocSearch-Button" aria-label="Search" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRoute, useRouter } from '#imports'
+import type { PropType } from 'vue'
+import { withoutTrailingSlash } from 'ufo'
+import { useRuntimeConfig } from 'nuxt3'
+import type { DocSearchOptions } from '../../types'
+// @ts-ignore - These are Nuxt3 aliases
+import { useRoute, useRouter, onMounted, watch, computed } from '#imports'
 
 const route = useRoute()
 
@@ -24,24 +18,50 @@ const router = useRouter()
 
 const props = defineProps({
   options: {
-    type: Object,
-    required: true
+    type: [Object, undefined] as PropType<DocSearchOptions | undefined>,
+    required: false,
+    default: undefined
   }
 })
 
-const isSpecialClick = event => event.button === 1 || event.altKey || event.ctrlKey || event.metaKey || event.shiftKey
+/**
+ * Try to grab options from runtimeConfig.
+ *
+ * If not found, fallback on props.
+ */
+const options = computed<DocSearchOptions>(
+  () => {
+    if (!props.options) {
+      const { algolia } = useRuntimeConfig()
 
-const stripTrailingSlash = url => url.replace(/\/$|\/(?=\?)|\/(?=#)/g, '')
+      if (algolia && algolia.docSearch) { return algolia.docSearch }
+    }
 
-const getRelativePath = (absoluteUrl) => {
+    return props.options
+  }
+)
+
+/**
+ * Check if event is special click to avoid closing the DocSearch too soon.
+ */
+const isSpecialClick = (event: MouseEvent) => event.button === 1 || event.altKey || event.ctrlKey || event.metaKey || event.shiftKey
+
+/**
+ * Gets the relative path from an absolute URL provided by the DocSearch instance.
+ */
+const getRelativePath = (absoluteUrl: string) => {
   const { pathname, hash } = new URL(absoluteUrl)
-  const url = pathname.replace('https://v3.nuxtjs.org', '/') + hash
-  return stripTrailingSlash(url)
+  const url = window.location.origin
+  const relativeUrl = pathname.replace(url, '/') + hash
+  return withoutTrailingSlash(relativeUrl)
 }
 
-const initialize = async (userOptions: any, code: string) => {
-  const lang = code || 'en-US'
-
+/**
+ * Initialize the DocSearch instance.
+ * @param userOptions
+ */
+const initialize = async (userOptions: DocSearchOptions) => {
+  // Import @docsearch at runtime
   const docsearch = await Promise.all([
     // @ts-ignore
     import(/* webpackChunkName: "docsearch" */ '@docsearch/js'),
@@ -49,72 +69,112 @@ const initialize = async (userOptions: any, code: string) => {
     import(/* webpackChunkName: "docsearch" */ '@docsearch/css')
   ]).then(([docsearch]) => docsearch.default)
 
+  // TODO: Maybe bind this with @nuxt/i18n ?
+  // Resolve lang
+  const lang = userOptions?.lang || 'en'
+  // Generate lang prefix
+  const langPrefix = `${userOptions.langAttribute || 'language'}:${lang}`
+  // Get facet filters
+  const userFacetFilters = userOptions.facetFilters || []
+
   // Create DocSearch instance
   docsearch({
-    ...userOptions,
+    /**
+     * Local implementation of this DocSearhch box uses a local element with an `docsearch` id.
+     */
     container: '#docsearch',
+    appId: userOptions.applicationId,
+    apiKey: userOptions.apiKey,
+    indexName: userOptions.indexName,
     searchParameters: {
-      ...((!lang)
-        ? {}
-        : {
-            facetFilters: [`${userOptions.langAttribute || 'language'}:${lang}`].concat(
-              userOptions.facetFilters || []
-            )
+      ...(
+        // Prefix facetFilters with langAttribute, otherwise use raw facetFilters
+        (!lang)
+          ? {
+              facetFilters: userFacetFilters
+            }
+          : {
+              facetFilters: [langPrefix].concat(userFacetFilters)
+            }
+      ),
+      ...userOptions.searchParameters
+    },
+    /**
+     * Transform items into relative URL format (compatibiltiy with Vue Router).
+     */
+    transformItems: userOptions.transformItems
+      ? userOptions.transformItems
+      : (items) => {
+          return items.map((item) => {
+            return {
+              ...item,
+              url: getRelativePath(item.url)
+            }
           })
-    },
-    navigator: {
-      navigate: ({ itemUrl }) => {
-        const { pathname: hitPathname } = new URL(window.location.origin + itemUrl)
-        // Vue Router doesn't handle same-page navigation so we use
-        // the native browser location API for anchor navigation.
-        if (route.path === hitPathname) {
-          window.location.assign(window.location.origin + itemUrl)
-        } else {
-          router.push(itemUrl)
-        }
-      }
-    },
-    transformItems: (items) => {
-      return items.map((item) => {
-        return {
-          ...item,
-          url: getRelativePath(item.url)
-        }
-      })
-    },
-    hitComponent: ({ hit, children }) => {
-      return {
-        type: 'a',
-        constructor: undefined,
-        __v: 1,
-        props: {
-          href: hit.url,
-          children,
-          onClick: (event) => {
-            if (isSpecialClick(event)) {
-              return
+        },
+    navigator: userOptions.navigator
+      ? userOptions.navigator
+      : {
+          navigate: ({ itemUrl }) => {
+            const { pathname: hitPathname } = new URL(window.location.origin + itemUrl)
+            // Vue Router doesn't handle same-page navigation so we use
+            // the native browser location API for anchor navigation.
+            if (route.path === hitPathname) {
+              window.location.assign(window.location.origin + itemUrl)
+            } else {
+              router.push(itemUrl)
             }
-            // We rely on the native link scrolling when user is
-            // already on the right anchor because Vue Router doesn't
-            // support duplicated history entries.
-            if (route.fullPath === hit.url) {
-              return
-            }
-            const { pathname: hitPathname } = new URL(window.location.origin + hit.url)
-            // If the hits goes to another page, we prevent the native link behavior
-            // to leverage the Vue Router loading feature.
-            if (route.path !== hitPathname) {
-              event.preventDefault()
-            }
-            router.push(hit.url)
           }
-        }
-      }
-    }
+        },
+    hitComponent: userOptions.hitComponent
+      ? userOptions.hitComponent
+      : ({ hit, children }) => {
+          return {
+            type: 'a',
+            constructor: undefined,
+            __v: 1,
+            props: {
+              href: hit.url,
+              children,
+              onClick: (event: MouseEvent) => {
+                if (isSpecialClick(event)) { return }
+
+                // We rely on the native link scrolling when user is
+                // already on the right anchor because Vue Router doesn't
+                // support duplicated history entries.
+                if (route.fullPath === hit.url) { return }
+
+                const { pathname: hitPathname } = new URL(window.location.origin + hit.url)
+
+                // If the hits goes to another page, we prevent the native link behavior
+                // to leverage the Vue Router loading feature.
+                if (route.path !== hitPathname) { event.preventDefault() }
+
+                router.push(hit.url)
+              }
+            }
+          }
+        },
+    // Spread user options, except the ones that are already used in the instance.
+    ...Object.entries(userOptions)
+      // Skip already used keys
+      .filter(
+        ([key]) => !['applicationId', 'apiKey', 'indexName', 'transformItems', 'navigator', 'hitComponent', 'facetFilters', 'langAttribute', 'lang'].includes(key)
+      )
+      // Recompose options
+      .reduce(
+        (acc, [key, value]) => {
+          acc[key] = value
+          return acc
+        },
+        {}
+      )
   })
 }
 
-onMounted(() => initialize(props.options, 'en-US'))
+// Initialize when mounted.
+onMounted(() => initialize(options.value))
 
-watch(() => props.options, (newValue: any) => initialize(newValue, 'en-US'))
+// Watch options and restart the instance if needed.
+watch(options, (n: any) => initialize(n))
 </script>
