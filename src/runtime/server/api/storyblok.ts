ed8316b5591f902e4074f637c981d8a74b452a81
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-11-16 16:34:18 +0100

    chore: bump to 3.0.0

--- src/runtime/server/api/storyblok.ts
+++ src/runtime/server/api/storyblok.ts
@@ -1,11 +1,11 @@
-import { defineEventHandler, useQuery } from 'h3'
+import { defineEventHandler, getQuery } from 'h3'
 import { useRuntimeConfig } from '#imports'
 import StoryblokAlgoliaIndexer from 'storyblok-algolia-indexer';
 
 const config = useRuntimeConfig()
 
 export default defineEventHandler((event) => {
-  const { secret } = useQuery(event)
+  const { secret } = getQuery(event)
 
   if (config.algoliaIndexer.storyblok.secret !== secret) return 'You are not allowed to access this resource'
 
