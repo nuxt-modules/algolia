03eee66d1a396126e6d76dcc3f00704c9da5a7c9
Matteo Rigoni <rigonimat@gmail.com>
2023-06-05 11:24:34 +0200

    Added key to useAsyncAlgoliaSearch
    Multiple useAsyncAlgoliaSearch calls would overwrite each other if done on same index. Added optional key parameter

--- src/runtime/composables/useAsyncAlgoliaSearch.ts
+++ src/runtime/composables/useAsyncAlgoliaSearch.ts
@@ -3,7 +3,7 @@ import type { RequestOptionsObject } from '../../types'
 import { useAlgoliaInitIndex } from './useAlgoliaInitIndex'
 import { useNuxtApp, useAsyncData, useRuntimeConfig } from '#imports'
 
-export type SearchParams = { query: string, indexName?: string } & RequestOptionsObject;
+export type SearchParams = { query: string, indexName?: string, key?: string } & RequestOptionsObject;
 
 export async function useAsyncAlgoliaSearch ({ query, requestOptions, indexName }: SearchParams) {
   const config = useRuntimeConfig();
@@ -13,7 +13,7 @@ export async function useAsyncAlgoliaSearch ({ query, requestOptions, indexName 
 
   const algoliaIndex = useAlgoliaInitIndex(index)
 
-  const result = await useAsyncData(`${index}-async-search-result`, async () => {
+  const result = await useAsyncData(`${index}-async-search-result-${key ?? ''}`, async () => {
     if (process.server) {
       const nuxtApp = useNuxtApp()
       nuxtApp.$algolia.transporter.requester = (await import('@algolia/requester-node-http').then(lib => lib.default || lib)).createNodeHttpRequester()
