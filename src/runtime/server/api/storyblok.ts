e9ad62dc8ed2176bbd54d6736de42c1d32134a50
Julien Testaz <julien.testaz@liip.ch>
2024-12-16 14:40:41 +0100

    feat: add a content version parameter and the possibility of making a custom config for storyblok

--- src/runtime/server/api/storyblok.ts
+++ src/runtime/server/api/storyblok.ts
@@ -1,19 +1,30 @@
 import { defineEventHandler, getQuery } from 'h3'
 import { useRuntimeConfig } from '#imports'
 import StoryblokAlgoliaIndexer from 'storyblok-algolia-indexer';
+import {createError} from "nuxt/app";
 
 const config = useRuntimeConfig()
 
 export default defineEventHandler((event) => {
   const { secret } = getQuery(event)
 
-  if (config.algoliaIndexer.storyblok.secret !== secret) return 'You are not allowed to access this resource'
+  if (config.algoliaIndexer.storyblok.secret !== secret) throw createError({
+    statusCode: 500,
+    message: 'Invalid secret',
+    fatal: true
+  })
 
   new StoryblokAlgoliaIndexer({
     algoliaAppId: config.public.algolia.applicationId,
     algoliaApiAdminToken: config.algoliaIndexer.storyblok.algoliaAdminApiKey,
     algoliaIndexName: config.algoliaIndexer.storyblok.indexName,
     storyblokContentDeliveryApiToken: config.algoliaIndexer.storyblok.accessToken,
+    options: {
+      per_page: 100,
+      page: 1,
+      version: config.algoliaIndexer.storyblok.contentVersion,
+      ...config.algoliaIndexer.storyblok.options
+    }
   })
 
   return 'Algolia indexed with the data from Storyblok!'
