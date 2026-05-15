71ed334c71d1ad54981942ae0058d2543c68f4d9
Domenik Reitzner <domenik@reitzner.at>
2026-05-15 10:34:10 +0200

    Update plugin with new algolia  version

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,23 +1,20 @@
-import { SearchClient } from 'algoliasearch/lite'
+import { createInMemoryCache } from '@algolia/cache-in-memory'
+import { createFetchRequester } from '@algolia/requester-fetch'
 import { defineNuxtPlugin, useRuntimeConfig } from '#imports'
-import { createInMemoryCache } from '@algolia/cache-in-memory';
-import { createFetchRequester } from '@algolia/requester-fetch';
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const { applicationId, apiKey, lite, recommend, cache } = useRuntimeConfig().public.algolia
 
-  // Have to import algoliasearch directly from esm.browser because algoliasearch by default provides umd.js file which causes Nuxt to throw error
-  // Also, cannot use simple string interpolation due to error 'Cannot read property 'stubModule' of undefined'
   const algoliasearch = lite
-    ? await import('algoliasearch/dist/algoliasearch-lite.esm.browser').then(lib => lib.default || lib)
-    : await import('algoliasearch/dist/algoliasearch.esm.browser').then(lib => lib.default || lib)
+    ? await import('algoliasearch/lite').then(lib => lib.liteClient)
+    : await import('algoliasearch').then(lib => lib.searchClient)
 
-  const algoliaSearchClient: SearchClient = cache ? algoliasearch(applicationId, apiKey, { responsesCache: createInMemoryCache(), requestsCache: createInMemoryCache({ serializable: false }), requester: createFetchRequester() }) : algoliasearch(applicationId, apiKey, { requester: createFetchRequester() })
+  const algoliaSearchClient = cache ? algoliasearch(applicationId, apiKey, { responsesCache: createInMemoryCache(), requestsCache: createInMemoryCache({ serializable: false }), requester: createFetchRequester() }) : algoliasearch(applicationId, apiKey, { requester: createFetchRequester() })
 
   nuxtApp.provide('algolia', algoliaSearchClient)
 
   if (recommend) {
-    const algoliaRecommend = await import('@algolia/recommend/dist/recommend.esm.browser').then(lib => lib.default || lib)
-    nuxtApp.provide('algoliaRecommend', algoliaRecommend(applicationId, apiKey))
+    const algoliaRecommend = await import('@algolia/recommend')
+    nuxtApp.provide('algoliaRecommend', algoliaRecommend.recommendClient(applicationId, apiKey))
   }
 })
